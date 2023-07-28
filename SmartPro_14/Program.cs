2226e165e09b22b09bc5542e1deb5c8c20e74cf8
lesponns <71814927+lesponns@users.noreply.github.com>
2023-07-28 07:27:24 +0300

    Update Program.cs

--- SmartPro_14/Program.cs
+++ SmartPro_14/Program.cs
@@ -1,20 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace SmartPro_14
-{
-    internal class Program
-    {
-        static void Main(string[] args)
+namespace SmartPro_14 {
+    internal class Program {
+        static void Main(string[] args) 
         {
-            
-            for (int i = 0; i < 5; i++) 
-            {
-                for(int j = 0; j <= i; j++) 
-                {
+            for (int i = 0; i < 5; i++) {
+                for(int j = 0; j <= i; j++) {
                     Console.Write("* ");
                 }
                 Console.WriteLine("");
